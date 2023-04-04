9a829b917ff87902ef71d94d7d614cd321d93b6d
Neil Angelo Santos <neil.angelo@hotmail.com>
2023-04-04 14:25:59 -0300

    initialGit with first itens

---
+++ ConsoleAppAula01/ConsoleAppAula01/Program.cs
@@ -0,0 +1,31 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+
+// Usuário digita 2 valores e retorna a média aritimética
+
+//Console.WriteLine("Insira o valor 1");
+//float v1 = float.Parse(Console.ReadLine());
+//Console.WriteLine("Insira o valor 2");
+//float v2 = float.Parse(Console.ReadLine());
+//Console.WriteLine("A média dos valores é: " + (v1 + v2) / 2);
+
+// Fazer a conversão de Celsius para Fahrenheit
+//Console.Write("Informe a temperatura em ºC: ");
+//double Celsius = double.Parse(Console.ReadLine());
+//double Fahrenheit = (Celsius * 9) / 5 + 32;
+//Console.WriteLine("A Temperatura em Fahrenheit é: " + Fahrenheit);
+
+// inverter os valores das variáveis
+int a = 10;
+int b = 20;
+Console.WriteLine("a: " + a);
+Console.WriteLine("b: " + b);
+int aux = a;
+a = b;
+b = aux;
+
+Console.WriteLine("Valores invertidos:");
+Console.WriteLine("a: " + a);
+Console.WriteLine("b: " + b);
+
+
