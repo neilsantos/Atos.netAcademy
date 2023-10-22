dda6c1fb621a7cc85bc8b7254c060029edf4c18d
Neil Angelo Santos <neil.angelo@hotmail.com>
2023-10-22 12:46:46 -0300

    Delete Atos.netAcademy-FinalProject directory

--- Atos.netAcademy-FinalProject/HomeManager/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace HomeManager.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
